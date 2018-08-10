bbdc824bbb545d8341a985d473e53a3a9aa214d4
708 <jarukit.r@ku.th>
2018-08-10 14:29:57 +0700

    add my initial code

---
+++ README.md
@@ -0,0 +1,5 @@
+#Data Structure
+
+by Jack (5910450999)
+
+This repository contains programs for data structure
---
+++ src/test/java/csku/datastructure/ArrayCollectionTest.java
@@ -0,0 +1,33 @@
+package csku.datastructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by 708 on 8/10/2018.
+ */
+class ArrayCollectionTest {
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void testSizeZero() {
+        ArrayCollection arrayCollection = new ArrayCollection();
+        assertEquals(0, arrayCollection.size());
+    }
+
+}
